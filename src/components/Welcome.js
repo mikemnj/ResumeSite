70fe648a472e529b55b6362339b71d8475112a62
MikeMcCully <mikemnj@udel.edu>
2024-01-18 15:31:26 -0500

    day 1

---
+++ src/components/Welcome.js
@@ -0,0 +1,16 @@
+//Create a page with a "Welcome" text, acompanied by
+//a button that says "ENTER" that loads MAIN
+function Welcome(){
+    return(
+        <div>
+            <header className="App-header">
+                Welcome.
+            </header>
+            
+            <button>
+                ENTER
+            </button>
+        </div>
+    );
+}
+export default Welcome;
